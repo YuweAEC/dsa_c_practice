b249ef13789a68b14e69ebc863baff0f8566943c
Yuvraj Singh <yuwe18b@gmail.com>
2023-06-24 11:50:45 +0530

    add codes to queue file

--- queue/queue.c
+++ queue/queue.c
@@ -1,3 +1,32 @@
 #include<stdio.h>
 #define MAX 5
 
+int a[MAX],f,r;
+
+void add(int x)
+{
+    if(r==MAX-1)
+    {
+        printf("THE QUEUE IS FULL");
+        return ;
+    }
+    if(f==-1)
+    f=r=0;
+    else
+    r++;
+    a[r]=x;
+}
+
+void deletion()
+{
+    if(f==-1)
+    {
+        printf("Q is EMpty, Can't delete");
+        return ;
+    }
+    printf("the deleted element is %d", a[f]);
+    if(f==r)
+    f=f=-1;
+    else
+    f++;
+}
