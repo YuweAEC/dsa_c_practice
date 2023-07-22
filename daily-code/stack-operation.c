f10aab5c1c14e7f3c354d195ab3fde920a8367e6
Yuvraj Singh <yuwe18b@gmail.com>
2023-07-22 21:07:22 +0530

    refactor: remove display and add veiw case

--- daily-code/stack-operation.c
+++ daily-code/stack-operation.c
@@ -7,35 +7,36 @@ void push();
 int pop();
 int stackArr[5], top=-1;
 
-void display()    // defining the display function used in push and pop
-{
-    printf("\n Stack Contains \n");
-    for(int i=top; i>=0; i--)
-    printf("%d \n", stackArr[i]);
-}
 
 void main()
 {
     int op, it, v;
     while (100) //Use an infinite loop to continuously read user input
     {
-        printf("\n 1-PUSH 2-POP 3-EXIT \n Enter the option");
+        printf("\n 1-PUSH \n 2-POP \n 3-Veiw-stack \n 4-EXIT \n Enter the option: ");
         scanf("%d", &op);
         switch (op)
         {
         case 1:     /* constant-expression */
-            printf("Enter the item to push:");
-            scanf("%d,", &it);
+            printf("\nEnter the item to push: ");
+            scanf("%d", &it);
             push(it);   /* invoke or calling the push function */
             break;
         case 2:
             v=pop();   /* invoke or calling the pop function */
             if (v>0)
             {
-                printf(" %d is pop from the stack", v);
+                printf("\n%d is pop from the stack", v);
             }
             break;
         case 3:
+            printf("\n Stack Contains:  ");
+            for (int i = 0; i <= top; i++)
+            {
+                printf("\n%d", stackArr[i]);
+            }
+            break;
+        case 4:
             exit(0);
         }
     }
@@ -52,20 +53,18 @@ void push(int x)    // push function defination
         top=top+1;
         stackArr[top]=x;    //assigning item on the top of the stack
     }
-    display();
 }
 
 int pop()     // pop function defination
 {
     if (top<0)
     {
-        printf("\n Underflow");
+        printf("\n Underflow ");
     }
     else
     {
         int v=stackArr[top];     //assiging value to pop form the stack
         top=top-1;      //decrease top by 1
-        display();
         return v;    
     }
 }
