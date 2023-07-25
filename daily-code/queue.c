fe6ae7e73271d042371f9922058a7a2f74b727c8
Yuvraj Singh <yuwe18b@gmail.com>
2023-07-25 22:39:28 +0530

    refactor: add new case for list and call them into main

--- daily-code/queue.c
+++ daily-code/queue.c
@@ -1,35 +1,60 @@
 #include<stdio.h>
-#define MAX 5
+#define MAX 5 
 
 void add(int x);
-void deletion();
-int a[MAX],front,rare;
+void deletion(); 
+int a[MAX],front,rare; 
 
-void add(int x) //for adding element in queue
+void add(int x) 
 {
-	if(rare==MAX-1)
+	if(rare==MAX-1) 		
 	{
 		printf("THE QUEUE IS FULL");
 		return;
 	}
 	if(front==-1)
-		front=rare=0;
-	else
+		front=rare=0; 
+	else  
 		rare++;
 	a[rare]=x;
 }
 	
-void deletion() //for deleting element from queue
+void deletion() 
 {
 	if(front==-1)
 	{
 		printf("Q is Empty, Can't delete");
 		return;
 	}
-	printf("the deleted element is %d",a[front]);
+	printf("the deleted element is %d",a[front]); 
 	if(front==rare)
-		front=rare=-1;
+		front=rare=-1; 
 	else
 		front++;
 }
 
+void main()	
+{
+	int ch,x;
+	front=rare=-1; 
+	do
+	{
+		printf("\n1. Add\n2. Delete\n3. List\n4. Exit\nEnter your choice: ");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1: printf("Enter the element to be added: "); 		
+					scanf("%d",&x);
+					add(x); 
+					break;
+			case 2: deletion(); 
+					break;
+			case 3: for(int i=front;i<=rare;i++)  	
+					{
+						printf("%d\t",a[i]); 
+					}
+			case 4: break;
+			default: printf("Invalid choice")
+		}
+	}while(ch!=3); 
+}
