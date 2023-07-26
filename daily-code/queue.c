72c6ea22fa1cc394b2a9a948f51acac6640ad409
Yuvraj Singh <yuwe18b@gmail.com>
2023-07-26 11:03:40 +0530

    refactor: rename the function queue and change name from rare to rear

--- daily-code/queue.c
+++ daily-code/queue.c
@@ -3,20 +3,20 @@
 
 void add(int x);
 void deletion(); 
-int a[MAX],front,rare;  //global declaration of queue
+int queue[MAX],front,rear;  //global declaration of queue
 
 void add(int x) 
 {
-	if(rare==MAX-1) //check if queue is full 		
+	if(rear==MAX-1) //check if queue is full 		
 	{
 		printf("THE QUEUE IS FULL");
 		return;
 	}
 	if(front==-1) //check if queue is empty
-		front=rare=0; 
+		front=rear=0; 
 	else  
-		rare++;
-	a[rare]=x;
+		rear++;
+	queue[rear]=x;
 }
 	
 void deletion() 
@@ -26,9 +26,9 @@ void deletion()
 		printf("Q is Empty, Can't delete");
 		return;
 	}
-	printf("the deleted element is %d",a[front]);  
-	if(front==rare)   //	check if only one element is present in queue
-		front=rare=-1; 
+	printf("the deleted element is %d",queue[front]);  
+	if(front==rear)   //	check if only one element is present in queue
+		front=rear=-1; 
 	else
 		front++;
 }
@@ -36,7 +36,7 @@ void deletion()
 void main()	
 {
 	int ch,x;  //ch is choice
-	front=rare=-1;  //initially queue is empty
+	front=rear=-1;  //initially queue is empty
 	do
 	{
 		printf("\n1. Add\n2. Delete\n3. List\n4. Exit\nEnter your choice: ");
@@ -49,9 +49,9 @@ void main()
 					break;
 			case 2: deletion();  //call delete function
 					break;
-			case 3: for(int i=front;i<=rare;i++)  	//display the queue
+			case 3: for(int i=front;i<=rear;i++)  	//display the queue
 					{
-						printf("%d\t",a[i]); 
+						printf("%d\t",queue[i]); 
 					}
 			case 4: break;
 			default: printf("Invalid choice");
