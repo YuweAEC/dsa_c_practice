d4bee00c8ced8341f56f3cf990a7bf1653d3c8d4
Yuvraj Singh <yuwe18b@gmail.com>
2023-07-29 23:57:46 +0530

    feat: add code to delete element from the linked list

--- daily-code/polish.c
+++ daily-code/polish.c
@@ -24,6 +24,39 @@ void createLindedList()
             h = t;
         }
         printf("Do you want to insert more elements(y/n):");
-        a=getchar();
-    } while (a == 'y'|| a=='Y');
+void display()
+{
+    node *h;
+    if (h == NULL)
+    {
+        printf("List is empty");
+        return;
+    }
+    printf("The Elements in the list are:");
+    while (h != NULL)
+    {
+        printf("%5d", h->data);
+        h = h->next;
+    }
+}
+
+void deleteElement()
+{
+    node *p, *k;
+    p=search(x);
+    if (p == NULL)
+    {
+        printf("Not found Element can't be deleted");
+        return;
+    }
+    if(head==p)
+        head=head->next;
+    else
+    {
+        k=head;
+        while(k->next !=p)
+            k=k->next;
+        k->next=p->next;
+    }
+    free(p);
 }
