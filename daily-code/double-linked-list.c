6cf63b254de57758b215fb336966f6ec9aa62f30
Yuvraj Singh <yuwe18b@gmail.com>
2023-08-22 23:42:02 +0530

    feat: add code to create or add elements in double linked list

--- daily-code/double-linked-list.c
+++ daily-code/double-linked-list.c
@@ -16,3 +16,58 @@ struct node
 
 struct node *start = NULL;
 
+int main()
+{
+    int choice;
+    while(1)
+    {
+        printf("\n1. Add");
+        printf("\n2. Display");
+        printf("\n3. Delete");
+        printf("\n4. Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            case 1:
+                add();
+                break;
+            case 2:
+                display();
+                break;
+            case 3:
+                delete();
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("\nInvalid choice");
+        }
+    }
+    return 0;
+}
+
+void add()
+{
+    struct node *temp, *p;
+    temp = (struct node *)malloc(sizeof(struct node));
+    printf("\nEnter data: ");
+    scanf("%d", &temp->data);
+    temp->next = NULL;
+    temp->prev = NULL;
+    if(start == NULL)
+    {
+        start = temp;
+    }
+    else
+    {
+        p = start;
+        while(p->next != NULL)
+        {
+            p = p->next;
+        }
+        p->next = temp;
+        temp->prev = p;
+    }
+}
+
