1526acfbd94fc29e2f92ebd09e92741c7c0f330a
Yuvraj Singh <yuwe18b@gmail.com>
2023-09-17 00:01:24 +0530

    refactor: inititla change in code lines and logic

--- daily-code/polish.c
+++ daily-code/polish.c
@@ -1,31 +1,61 @@
 #include<stdio.h>
 #include<conio.h>
+// program to perform operations on polish notation
+
+
+
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} node;
+
+int main()
+{
+    int ch;
+    while (1)
+    {
+        printf("\n1.Create\n2.Display\n3.Delete\n4.Exit\n");
+        printf("\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            createLindedList();
+            break;
+        case 2:
+            display();
+            break;
+        case 3:
+            deleteElement();
+            break;
+        case 4:
+            exit(0);
+        default:
+            printf("Invalid choice");
+        }
+    }
+    return 0;
+}
 
 void createLindedList()
 {
-    int x ;
-    char a;
-    node *t, *h;
-    do
+    node *h, *p;
+    int x;
+    printf("Enter the data for the node");
+    scanf("%d", &x);
+    p = (node *)malloc(sizeof(node));
+    p->data = x;
+    p->next = NULL;
+    if (h == NULL)
     {
-        printf("Enter the element to be inserted:");
-        scanf("%d", &x);
-        t = (node *x)malloc(sizeof(node));
-        t->data = x;
-        t->next = NULL;
-        if (h == NULL)
-        {
-            head=t;
-            h = t;
-        }
-        else
-        {
-            h->next = t;
-            h = t;
-        }
-        printf("Do you want to insert more elements(y/n):");
+        h = p;
+        return;
     }
-    while (a == 'y');
+    while (h->next != NULL)
+        h = h->next;
+    h->next = p;
 }
 
 void display()
@@ -46,21 +76,32 @@ void display()
 
 void deleteElement()
 {
-    node *p, *k;
-    p=search(x);
-    if (p == NULL)
+    node *h, *p;
+    int x;
+    if (h == NULL)
     {
-        printf("Not found Element can't be deleted");
+        printf("List is empty");
         return;
     }
-    if(head==p)
-        head=head->next;
-    else
+    printf("Enter the element to be deleted");
+    scanf("%d", &x);
+    if (h->data == x)
     {
-        k=head;
-        while(k->next !=p)
-            k=k->next;
-        k->next=p->next;
+        p = h;
+        h = h->next;
+        free(p);
+        return;
     }
-    free(p);
+    while (h->next != NULL)
+    {
+        if (h->next->data == x)
+        {
+            p = h->next;
+            h->next = p->next;
+            free(p);
+            return;
+        }
+        h = h->next;
+    }
+    printf("Element not found");
 }
