923f00c31c01b95e9d2be8b46798d042e78dddb6
Yuvraj Singh <yuwe18b@gmail.com>
2023-09-18 23:26:59 +0530

    feat: revise the circular queue codewq

--- queue/circular-queue.c
+++ queue/circular-queue.c
@@ -1,6 +1,4 @@
-// program to element circular queue
 #include<stdio.h>
-#include<conio.h>
 #include<stdlib.h>
 #define MAX 5
 
@@ -8,19 +6,19 @@ void add(int x);
 void deletion();
 void display();
 
-int main()
-{
+int f = -1, r = -1; // Initialize front and rear indices
+int a[MAX]; // Queue array
+
+int main() {
     int ch, x;
-    while(1)
-    {
+    while (1) {
         printf("\n1. Add");
         printf("\n2. Delete");
         printf("\n3. Display");
         printf("\n4. Exit");
         printf("\nEnter your choice: ");
         scanf("%d", &ch);
-        switch(ch)
-        {
+        switch (ch) {
             case 1:
                 printf("\nEnter the element to be added: ");
                 scanf("%d", &x);
@@ -41,51 +39,52 @@ int main()
     return 0;
 }
 
-void add(int x)
-{
-    int r, f , a[MAX];
-    if(r==MAX-1)
-    {
+void add(int x) {
+    if ((r == MAX - 1 && f == 0) || (r + 1 == f)) {
         printf("THE QUEUE IS FULL");
-        return ;
+        return;
     }
 
-    if(f==-1)
-        f=r=0;
-    
+    if (f == -1)
+        f = r = 0;
+    else if (r == MAX - 1)
+        r = 0;
     else
         r++;
-    a[r]=x;
+
+    a[r] = x;
 }
 
-void deletion()
-{
-    int r, f , a[MAX];
-    if(f==-1)
-    {
-        printf("Q is EMpty, Can't delete");
-        return ;
+void deletion() {
+    if (f == -1) {
+        printf("Queue is Empty, Can't delete");
+        return;
     }
-    
-    printf("the deleted element is %d", a[f]);
-    
-    if(f==r)
-        f=f=-1;
-    
+
+    printf("The deleted element is %d", a[f]);
+
+    if (f == r)
+        f = r = -1;
+    else if (f == MAX - 1)
+        f = 0;
     else
         f++;
 }
 
-void display()
-{
-    int r, f , a[MAX];
-    if(f==-1)
-    {
-        printf("Q is EMpty, Can't delete");
-        return ;
+void display() {
+    if (f == -1) {
+        printf("Queue is Empty, Can't display");
+        return;
     }
-    
-    for(int i=f; i<=r; i++)
-        printf("%d", a[i]);
-}
 
+    int i = f;
+    do {
+        printf("%d ", a[i]);
+        if (i == r && r != MAX - 1)
+            break;
+        if (i == MAX - 1)
+            i = 0;
+        else
+            i++;
+    } while (i != (r + 1) % MAX);
+}
