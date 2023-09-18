b769ec85ee3e954d6713a2f22d05df2597757b2c
Yuvraj Singh <yuwe18b@gmail.com>
2023-09-18 23:18:11 +0530

    feat: add code for main function

--- queue/circular-queue.c
+++ queue/circular-queue.c
@@ -8,6 +8,39 @@ void add(int x);
 void deletion();
 void display();
 
+int main()
+{
+    int ch, x;
+    while(1)
+    {
+        printf("\n1. Add");
+        printf("\n2. Delete");
+        printf("\n3. Display");
+        printf("\n4. Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch(ch)
+        {
+            case 1:
+                printf("\nEnter the element to be added: ");
+                scanf("%d", &x);
+                add(x);
+                break;
+            case 2:
+                deletion();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("\nInvalid choice");
+        }
+    }
+    return 0;
+}
+
 void add(int x)
 {
     int r, f , a[MAX];
