eb243b29a688539733a4e053696c7b6289a8d666
Yuvraj Singh <yuwe18b@gmail.com>
2023-09-19 23:10:01 +0530

    feat: comment logic of the code on program

--- daily-code/polynomial.c
+++ daily-code/polynomial.c
@@ -3,18 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node
+struct node  // Structure of a node in a linked list 
 {
     int coeff;
     int exp;
     struct node *next;
 };
 
-struct node *head1 = NULL;
-struct node *head2 = NULL;
+struct node *head1 = NULL; // Global pointers to the heads of the two polynomials 
+struct node *head2 = NULL; 
 struct node *head3 = NULL;
 
-void create(struct node **head)
+void create(struct node **head)  // Function to create a polynomial 
 {
     struct node *newnode, *temp;
     int n, i;
@@ -22,7 +22,7 @@ void create(struct node **head)
     scanf("%d", &n);
     for (i = 0; i < n; i++)
     {
-        newnode = (struct node *)malloc(sizeof(struct node));
+        newnode = (struct node *)malloc(sizeof(struct node)); // Create a new node 
         printf("Enter the coefficient and exponent: ");
         scanf("%d%d", &newnode->coeff, &newnode->exp);
         newnode->next = NULL;
@@ -39,27 +39,27 @@ void create(struct node **head)
     }
 }
 
-void display(struct node *head)
+void display(struct node *head) // Function to display a polynomial 
 {
     struct node *temp;
     temp = head;
     while (temp != NULL)
     {
-        printf("%dx^%d", temp->coeff, temp->exp);
+        printf("%dx^%d", temp->coeff, temp->exp);  // Display the coefficient and exponent of each term 
         temp = temp->next;
         if (temp != NULL)
             printf(" + ");
     }
 }
 
-void sumofPolynomials(struct node *head1, struct node *head2, struct node **head3)
+void sumofPolynomials(struct node *head1, struct node *head2, struct node **head3) // Function to add two polynomials 
 {
     struct node *temp1, *temp2, *temp3, *newnode;
     temp1 = head1;
     temp2 = head2;
     while (temp1 != NULL && temp2 != NULL)
     {
-        newnode = (struct node *)malloc(sizeof(struct node));
+        newnode = (struct node *)malloc(sizeof(struct node)); // Create a new node 
         newnode->next = NULL;
         if (*head3 == NULL)
         {
@@ -73,7 +73,7 @@ void sumofPolynomials(struct node *head1, struct node *head2, struct node **head
         }
         if (temp1->exp == temp2->exp)
         {
-            temp3->coeff = temp1->coeff + temp2->coeff;
+            temp3->coeff = temp1->coeff + temp2->coeff; // Add the coefficients of the terms with the same exponent 
             temp3->exp = temp1->exp;
             temp1 = temp1->next;
             temp2 = temp2->next;
@@ -93,7 +93,7 @@ void sumofPolynomials(struct node *head1, struct node *head2, struct node **head
     }
     while (temp1 != NULL)
     {
-        newnode = (struct node *)malloc(sizeof(struct node));
+        newnode = (struct node *)malloc(sizeof(struct node)); // Create a new node 
         newnode->next = NULL;
         temp3->next = newnode;
         temp3 = newnode;
@@ -113,7 +113,7 @@ void sumofPolynomials(struct node *head1, struct node *head2, struct node **head
     }
 }
 
-int main()
+int main() // Main function 
 {
     printf("Enter the first polynomial:\n");
     create(&head1);
@@ -126,5 +126,5 @@ int main()
     sumofPolynomials(head1, head2, &head3);
     printf("\nThe sum of the two polynomials is: ");
     display(head3);
-    return 0;
+    return 0; // End of main function 
 }
