245ca7610e7f4d111fce4fce993edb0bc00a4740
Yuvraj Singh <yuwe18b@gmail.com>
2023-09-19 23:07:31 +0530

    feat: add new code to fu=ind the sum of both the polynomials entered and main

--- daily-code/polynomial.c
+++ daily-code/polynomial.c
@@ -52,3 +52,79 @@ void display(struct node *head)
     }
 }
 
+void sumofPolynomials(struct node *head1, struct node *head2, struct node **head3)
+{
+    struct node *temp1, *temp2, *temp3, *newnode;
+    temp1 = head1;
+    temp2 = head2;
+    while (temp1 != NULL && temp2 != NULL)
+    {
+        newnode = (struct node *)malloc(sizeof(struct node));
+        newnode->next = NULL;
+        if (*head3 == NULL)
+        {
+            *head3 = newnode;
+            temp3 = newnode;
+        }
+        else
+        {
+            temp3->next = newnode;
+            temp3 = newnode;
+        }
+        if (temp1->exp == temp2->exp)
+        {
+            temp3->coeff = temp1->coeff + temp2->coeff;
+            temp3->exp = temp1->exp;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        else if (temp1->exp > temp2->exp)
+        {
+            temp3->coeff = temp1->coeff;
+            temp3->exp = temp1->exp;
+            temp1 = temp1->next;
+        }
+        else
+        {
+            temp3->coeff = temp2->coeff;
+            temp3->exp = temp2->exp;
+            temp2 = temp2->next;
+        }
+    }
+    while (temp1 != NULL)
+    {
+        newnode = (struct node *)malloc(sizeof(struct node));
+        newnode->next = NULL;
+        temp3->next = newnode;
+        temp3 = newnode;
+        temp3->coeff = temp1->coeff;
+        temp3->exp = temp1->exp;
+        temp1 = temp1->next;
+    }
+    while (temp2 != NULL)
+    {
+        newnode = (struct node *)malloc(sizeof(struct node));
+        newnode->next = NULL;
+        temp3->next = newnode;
+        temp3 = newnode;
+        temp3->coeff = temp2->coeff;
+        temp3->exp = temp2->exp;
+        temp2 = temp2->next;
+    }
+}
+
+int main()
+{
+    printf("Enter the first polynomial:\n");
+    create(&head1);
+    printf("Enter the second polynomial:\n");
+    create(&head2);
+    printf("The first polynomial is: ");
+    display(head1);
+    printf("\nThe second polynomial is: ");
+    display(head2);
+    sumofPolynomials(head1, head2, &head3);
+    printf("\nThe sum of the two polynomials is: ");
+    display(head3);
+    return 0;
+}
