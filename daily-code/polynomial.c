808ad615b78c63fef390d91f7769f63281876fa0
Yuvraj Singh <yuwe18b@gmail.com>
2023-09-19 23:03:07 +0530

    feat: add code to create node for polynomial implementation

---
+++ daily-code/polynomial.c
@@ -0,0 +1,41 @@
+// write polynomial function on linked list 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int coeff;
+    int exp;
+    struct node *next;
+};
+
+struct node *head1 = NULL;
+struct node *head2 = NULL;
+struct node *head3 = NULL;
+
+void create(struct node **head)
+{
+    struct node *newnode, *temp;
+    int n, i;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        newnode = (struct node *)malloc(sizeof(struct node));
+        printf("Enter the coefficient and exponent: ");
+        scanf("%d%d", &newnode->coeff, &newnode->exp);
+        newnode->next = NULL;
+        if (*head == NULL)
+        {
+            *head = newnode;
+            temp = newnode;
+        }
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+    }
+}
+
