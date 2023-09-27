393a272300e013352ada933fb0d804fcd3e9e897
Yuvraj Singh <yuwe18b@gmail.com>
2023-09-27 23:43:46 +0530

    feat: add main function to doubly linked list code

--- daily-code/double-LL.c
+++ daily-code/double-LL.c
@@ -8,3 +8,28 @@ struct node {
 };
 
 struct node* start = NULL;
+
+void add();
+void deleteNode();
+
+int main() {
+    int ch;
+    while (1) {
+        printf("\n1. Add\n2. Delete\n3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+        switch (ch) {
+            case 1:
+                add();
+                break;
+            case 2:
+                deleteNode();
+                break;
+            case 3:
+                exit(0);
+            default:
+                printf("\nInvalid choice");
+        }
+    }
+    return 0;
+}
